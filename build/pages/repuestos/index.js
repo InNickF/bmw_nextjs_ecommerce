47c3ee16c95dce32ff0b570e189522fbb2593522
Julio Cesar <llulioscesar@gmail.com>
2020-08-20 14:47:16 -0500

    Pagina Repuestos

---
+++ build/pages/repuestos/index.js
@@ -0,0 +1,98 @@
+import React, {Fragment} from 'react'
+import Helmet from "react-helmet";
+import {Wrapper, Column, Row} from "../../common/src/components";
+import ContainerText from "../../styles/privacy";
+import {TitleContainer} from "../../styles/terms";
+
+const Repuestos = () => {
+
+	return (
+		<Fragment>
+			<Helmet>
+				<title>BMW Repuestos</title>
+			</Helmet>
+			<Wrapper>
+				<TitleContainer>
+					<h1>
+						REPUESTOS ORIGINALES PARA TU BMW.
+					</h1>
+				</TitleContainer>
+				<ContainerText>
+					<p style={{marginTop: "20px"}}>
+						Con BMW Shop podrás tener la mejor asesoría sin salir de casa, para que encuentres repuestos que
+						te proporcionan un rendimiento óptimo, gracias al desarrollo tecnológico y el cumplimiento de
+						estrictos estándares de calidad de BMW GROUP. Nuestro compromiso es brindarte la mejor
+						experiencia dentro y fuera de tu carro, por eso todos nuestros repuestos cuentan con una
+						garantía de 24 meses.
+					</p>
+				</ContainerText>
+				<ContainerText>
+					<Row>
+						<Column>
+							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "center"}}>Baterías BMW</h2>
+							<p style={{padding: "0 16px"}}>
+								Cuenta con la tecnología EfficientDynamics, la cual ofrece una gestión inteligente de la
+								energía y actualmente garantiza una movilidad sustentable. La batería BMW tiene una
+								larga vida útil, aún después de periodos largos de inmovilización. Permite mantener un
+								excelente arranque en motores que requieren mayor corriente, como los motores diesel de
+								alto rendimiento.
+							</p>
+							<img style={{maxHeight: "292px"}}
+								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r1.jpg" width="100%"/>
+						</Column>
+						<Column>
+							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "center"}}>Aceite BMW</h2>
+							<p style={{padding: "0 16px"}}>
+								Responde a las exigencias de los motores de BMW permitiendo así un alto rendimiento,
+								eficiencia y duración. Ofrece una mayor viscosidad, lo cual mejora los arranques en
+								frío. Gracias al desarrollo exclusivo y de alta tecnología permite reducir el consumo de
+								combustible, prolonga la vida útil del motor, y además, mantiene una limpieza activa que
+								protege las piezas principales.
+							</p>
+							<img style={{maxHeight: "292px"}}
+								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r2.jpg" width="100%"/>
+						</Column>
+					</Row>
+				</ContainerText>
+
+				<ContainerText>
+					<Row>
+						<Column>
+							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "center"}}>Escobillas
+								BMW</h2>
+							<p style={{padding: "0 16px"}}>
+								Con las escobillas BMW podrás mantener siempre una visibilidad y seguridad óptimas,
+								incluso en situaciones críticas. Tienen un rendimiento perfecto durante el limpiado
+								gracias a la distribución uniforme de las fuerzas y la elevada presión. Son capaces de
+								reconocer, gracias a la tecnología infrarroja, las gotas de lluvia que caen sobre el
+								parabrisas, lo que le permite adaptar la velocidad de los limpiaparabrisas de acuerdo
+								con la intensidad de la lluvia.
+							</p>
+							<img style={{maxHeight: "292px"}}
+								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r3.jpg" width="100%"/>
+						</Column>
+						<Column>
+							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "center"}}>Firenos BMW</h2>
+							<p style={{padding: "0 16px"}}>
+								Todos nuestros modelos están equipados por este sistema tanto en el eje delantero como
+								en el trasero, esto es debido a las grandes ventajas del sistema como, mejor frenada en
+								condiciones adversas, mejora en la disipación del calor.
+							</p>
+							<img style={{maxHeight: "292px"}}
+								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r4.jpg" width="100%"/>
+						</Column>
+					</Row>
+				</ContainerText>
+
+				<ContainerText>
+					<p style={{marginTop: "20px"}}>Puedes recibir asesoría ó realizar tu compra asistida a través de nuestro WhatsApp y nuestro chat
+						online.</p>
+				</ContainerText>
+
+			</Wrapper>
+		</Fragment>
+	)
+
+}
+
+export default Repuestos;
