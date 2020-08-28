5c2e54a488221284fe2656a7397df93929f71820
Julio Cesar <llulioscesar@gmail.com>
2020-08-28 13:44:45 -0500

    Texto pagina Repuestos

--- build/pages/repuestos/index.js
+++ build/pages/repuestos/index.js
@@ -3,7 +3,7 @@ import Helmet from "react-helmet";
 import {Wrapper, Column} from "../../common/src/components";
 import ContainerText from "../../styles/privacy";
 import {TitleContainer} from "../../styles/terms";
-import {Container, HeroContent, Inner, Row, ColLg12, Boxes, SmartBox} from "../../styles/repuestos";
+import {Container, HeroContent, Inner, Row, ColLg12, Boxes, SmartBox, Features} from "../../styles/repuestos";
 
 const Repuestos = () => {
 
@@ -23,9 +23,9 @@ const Repuestos = () => {
 								<h2 className="text-uppercase bold" style={{color: 'white'}}>
 									para tu BMW.
 								</h2>
-								<p className="mt-2">
+								{/*<p className="mt-2">
 									Con BMW Shop podrás tener la mejor asesoría sin salir de casa.
-								</p>
+								</p>*/}
 								<Boxes className="push-lg-2">
 									<Row>
 										<div className="col-lg-4 col-md-6 mb-5">
@@ -85,120 +85,115 @@ const Repuestos = () => {
 					<div className="box-overlay"/>
 				</Inner>
 			</HeroContent>
-			<Wrapper>
-				<TitleContainer>
-					<div style={{
-						background: "url(https://repuestos-landing.s3.us-east-2.amazonaws.com/banner-bmw.jpg)",
-						minHeight: "350px",
-						backgroundSize: "cover",
-						backgroundRepeat: "no-repeat",
-						backgroundPosition: "center",
-						display: "flex",
-						alignItems: "center",
-					}}>
-						<h1 style={{
-							paddingLeft: "16px",
-							color: "white",
-							fontWeight: "bold!important",
-							textShadow: "rgb(0, 0, 0) 2px 0px 4px",
-							fontFamily: "BMWGroup-Bold',sans-serif !important;"
-						}}>
-							REPUESTOS ORIGINALES PARA TU BMW.
-						</h1>
-					</div>
-				</TitleContainer>
-				<ContainerText>
-					<p style={{marginTop: "20px"}}>
+			<Features>
+				<Container>
+
+					<h2>
 						Con BMW Shop podrás tener la mejor asesoría sin salir de casa, para que encuentres repuestos que
-						proporcionan un rendimiento óptimo a tu BMW, gracias al desarrollo tecnológico y el cumplimiento de
-						estrictos estándares de calidad de BMW GROUP. Nuestro compromiso es brindarte la mejor
-						experiencia de compra, por eso todos nuestros repuestos cuentan con una
-						garantía de 24 meses por defecto de producto.
-					</p>
-				</ContainerText>
-				<ContainerText>
-					<Row>
-						<Column style={{
-							display: "flex",
-							flexDirection: "column",
-							justifyContent: "space-between",
-						}}>
-							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "left"}}>Baterías BMW</h2>
-							<p style={{padding: "0"}}>
-								Cuenta con la tecnología EfficientDynamics, la cual ofrece una gestión inteligente de la
-								energía y actualmente garantiza una movilidad sustentable. La batería BMW tiene una
-								larga vida útil, aún después de periodos largos de inmovilización. Permite mantener un
-								excelente arranque en motores que requieren mayor corriente, como los motores diesel de
-								alto rendimiento.
-							</p>
-							<img style={{maxHeight: "292px"}}
-								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r1.jpg" width="100%"/>
-						</Column>
-						<Column style={{
-							display: "flex",
-							flexDirection: "column",
-							justifyContent: "space-between",
-						}}>
-							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "left"}}>Aceite BMW</h2>
-							<p style={{padding: "0"}}>
-								Responde a las exigencias de los motores de BMW permitiendo así un alto rendimiento,
-								eficiencia y duración. Ofrece una mayor viscosidad, lo cual mejora los arranques en
-								frío. Gracias al desarrollo exclusivo y de alta tecnología permite reducir el consumo de
-								combustible, prolonga la vida útil del motor, y además, mantiene una limpieza activa que
-								protege las principales piezas del motor.
-							</p>
-							<img style={{maxHeight: "292px"}}
-								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r2.jpg" width="100%"/>
-						</Column>
+						proporcionan un rendimiento óptimo a tu BMW, gracias al desarrollo tecnológico y el cumplimiento
+						de estrictos estándares de calidad de BMW GROUP. Nuestro compromiso es brindarte la mejor
+						experiencia de compra, por eso todos nuestros repuestos cuentan con una garantía de 24 meses por
+						defecto de producto.
+					</h2>
+
+					<Row style={{marginTop: "5rem"}}>
+						<div className="col-lg-6 col-md-6 mb-5">
+							<SmartBox>
+								<Row>
+									<ColLg12>
+										<p className="bold" style={{textAlign: 'left'}}>
+											BATERÍAS BMW
+										</p>
+									</ColLg12>
+									<ColLg12>
+										<p style={{textAlign: 'left'}}>
+											Cuenta con la tecnología EfficientDynamics, la cual ofrece una gestión
+											inteligente de la energía y actualmente garantiza una movilidad sustentable.
+											La batería BMW tiene una larga vida útil, aún después de periodos largos de
+											inmovilización. Permite mantener un excelente arranque en motores que
+											requieren mayor corriente, como los motores diesel de alto rendimiento.
+										</p>
+										<img style={{maxHeight: "292px"}}
+											 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r1.jpg"
+											 width="100%"/>
+									</ColLg12>
+								</Row>
+							</SmartBox>
+						</div>
+						<div className="col-lg-6 col-md-6 mb-5">
+							<SmartBox>
+								<Row>
+									<ColLg12>
+										<p className="bold" style={{textAlign: 'left'}}>
+											ACEITE BMW
+										</p>
+									</ColLg12>
+									<ColLg12>
+										<p style={{textAlign: 'left'}}>
+											Responde a las exigencias de los motores de BMW permitiendo así un alto
+											rendimiento, eficiencia y duración. Ofrece una mayor viscosidad, lo cual
+											mejora los arranques en frío. Gracias al desarrollo exclusivo y de alta
+											tecnología permite reducir el consumo de combustible, prolonga la vida útil
+											del motor, y además, mantiene una limpieza activa que protege las
+											principales piezas del motor.
+										</p>
+										<img style={{maxHeight: "292px"}}
+											 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r2.jpg"
+											 width="100%"/>
+									</ColLg12>
+								</Row>
+							</SmartBox>
+						</div>
+						<div className="col-lg-6 col-md-6 mb-sm-5">
+							<SmartBox>
+								<Row>
+									<ColLg12>
+										<p className="bold" style={{textAlign: 'left'}}>
+											PLUMILLAS BMW
+										</p>
+									</ColLg12>
+									<ColLg12>
+										<p style={{textAlign: 'left'}}>
+											Con las plumillas BMW podrás mantener siempre una visibilidad y seguridad
+											óptimas, incluso en situaciones críticas. Tienen un rendimiento perfecto
+											durante el limpiado gracias a la distribución uniforme de las fuerzas y la
+											elevada presión. Son capaces de reconocer, gracias a la tecnología
+											infrarroja, las gotas de lluvia que caen sobre el parabrisas, lo que le
+											permite adaptar la velocidad de los limpiaparabrisas de acuerdo con la
+											intensidad de la lluvia.
+										</p>
+										<img style={{maxHeight: "292px"}}
+											 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r3.jpg"
+											 width="100%"/>
+									</ColLg12>
+								</Row>
+							</SmartBox>
+						</div>
+						<div className="col-lg-6 col-md-6 mb-sm-5">
+							<SmartBox>
+								<Row>
+									<ColLg12>
+										<p className="bold" style={{textAlign: 'left'}}>
+											PASTILLAS DE FRENO BMW
+										</p>
+									</ColLg12>
+									<ColLg12>
+										<p style={{textAlign: 'left'}}>
+											Garantizan la óptima interacción con el sistema de frenos, gracias a la
+											combinación de materiales que brindan los mejores valores en respuesta y
+											máximo confort. Se caracterizan por ofrecer gran desempeño en cualquier
+											condición climática.
+										</p>
+										<img style={{maxHeight: "292px"}}
+											 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmwr4.png"
+											 width="100%"/>
+									</ColLg12>
+								</Row>
+							</SmartBox>
+						</div>
 					</Row>
-				</ContainerText>
-
-				<ContainerText>
-					<Row>
-						<Column style={{
-							display: "flex",
-							flexDirection: "column",
-							justifyContent: "space-between",
-						}}>
-							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "left"}}>
-								Plumillas BMW
-							</h2>
-							<p style={{padding: "0"}}>
-								Con las plumillas BMW podrás mantener siempre una visibilidad y seguridad óptimas,
-								incluso en situaciones críticas. Tienen un rendimiento perfecto durante el limpiado
-								gracias a la distribución uniforme de las fuerzas y la elevada presión. Son capaces de
-								reconocer, gracias a la tecnología infrarroja, las gotas de lluvia que caen sobre el
-								parabrisas, lo que le permite adaptar la velocidad de los limpiaparabrisas de acuerdo
-								con la intensidad de la lluvia.
-							</p>
-							<img style={{maxHeight: "292px"}}
-								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmw+r3.jpg" width="100%"/>
-						</Column>
-						<Column style={{
-							display: "flex",
-							flexDirection: "column",
-							justifyContent: "space-between",
-						}}>
-							<h2 style={{marginTop: "30px", marginBottom: "20px", textAlign: "left"}}>
-								Pastillas de freno BMW</h2>
-							<p style={{padding: "0"}}>
-								Garantizan la óptima interacción con el sistema de frenos, gracias a la combinación de 
-								materiales que brindan los mejores valores en respuesta y máximo confort. Se caracterizan
-								por ofrecer gran desempeño en cualquier condición climática.
-							</p>
-							<img style={{maxHeight: "292px"}}
-								 src="https://repuestos-landing.s3.us-east-2.amazonaws.com/bmwr4.png" width="100%"/>
-						</Column>
-					</Row>
-				</ContainerText>
-
-				<ContainerText>
-					<p style={{marginTop: "20px"}}>Puedes recibir asesoría ó realizar tu compra asistida a través de
-						nuestro WhatsApp y nuestro chat
-						online.</p>
-				</ContainerText>
-
-			</Wrapper>
+				</Container>
+			</Features>
 		</Fragment>
 	)
 
